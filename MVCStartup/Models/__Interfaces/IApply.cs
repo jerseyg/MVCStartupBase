41d6887ddc6fd6f93e7df47f7fdb1727bcd70959
unknown <Jersey.Galapon@Work-PC.Oasis.domain>
2014-04-07 17:09:41 -0500

    Created new files for interface classes

---
+++ MVCStartup/Models/__Interfaces/IApply.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCStartup.Models.__Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IApplyDb<T>
+    {
+        void ApplyTo(T item);
+
+    }
+}
---
+++ MVCStartup/Models/__Interfaces/IRetrieve.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCStartup.Models.__Interfaces
+{
+    /// <summary>
+    /// Generic interface with an enitity model
+    /// </summary>
+    /// <typeparam name="Model">The Entity Model</typeparam>
+    /// <typeparam name="Type">Value Type</typeparam>
+    public interface IRetrieveDb<Model, Type>
+    {       
+        /// <summary>
+        /// Generic void method for retrieving from an enitity model
+        /// </summary>
+        /// <param name="value">Generic arugment type specified by the interface</param>
+        /// <returns>Entity Model</returns>
+        Model RetrieveFrom(Type value);
+    }
+}
