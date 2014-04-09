d6f148f78da35281e98640f5cc43ac8e8e8c1d45
unknown <Jersey.Galapon@Work-PC.Oasis.domain>
2014-04-09 11:10:58 -0500

    Revert "Revert "Base commit""
    This reverts commit 1c072a21ac637ed2198ac28ffc22d8d76da69991.

---
+++ MVCStartup/Controllers/Attributes/ValidateUserLogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MVCStartup.Models;
+using System.Web.Mvc;
+using MVCStartup.Models.__User__;
+using System.Web;
+
+namespace MVCStartup.Controllers.Attributes
+{
+        [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
+        sealed class ValidateUserLogin : ActionFilterAttribute
+        {
+            public override void OnActionExecuted(ActionExecutedContext filterContext)
+            {
+                var sessionValid = USession.CurrentUser.SessionId;
+                if (sessionValid != null) { }
+                else { filterContext.Result = new RedirectResult("/App/Login"); }
+            }
+    }
+}
